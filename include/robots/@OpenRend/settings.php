72801d59f83650f7c7b964eb1fa62eda3d5748ba
Dr. Xaies <mynamesnot@2240>
2018-04-13 09:00:14 +1000

    Robots API PHP in Social Media API's

--- include/robots/@OpenRend/settings.php
+++ include/robots/@OpenRend/settings.php
@@ -1,9 +1,9 @@
 <?php
 
-    return array(   'twitter'      =>  array(   'consumer_key'                  =>  'oclbkcn7QZ5fKBCZw2xCK0QqH',
-                                                'consumer_secret'               =>  'q0EHWHcffyddjyhh0UYIyHtZwkwDTjPiRXukwmefL8OzOjjyTj',
-                                                'oauth_access_token'            =>  '2836500978-WqXCo7D3vMKHaYKKLdVF5o6ev39QCZMqhSJ9Sq6',
-                                                'oauth_access_token_secret'     =>  'n4RHXBolb9dM0kP5GWbzzW5YqGxTDdEDuNnQ4OwyKv2mw',
+    return array(   'twitter'      =>  array(   'consumer_key'                  =>  'oclbkcn7QZ5fKBCZ----K0QqH',
+                                                'consumer_secret'               =>  'q0EHWHcffyddj----UYIyHtZwkwDTjPiRXukwmefL8OzOjjyTj',
+                                                'oauth_access_token'            =>  '2836500978----Co7D3vMKHaYKKLdVF5o6ev39QCZMqhSJ9Sq6',
+                                                'oauth_access_token_secret'     =>  'n4RHXBolb9dM0kP5GW----5YqGxTDdEDuNnQ4OwyKv2mw',
                                                 'username'                      =>  'OpenRend'
                                    ),
                     'robots'       =>   array(  'twitter'           =>  array('retweeter', 'replier', 'newfollowers', 'thankfollowers', 'fairwellfollowers')
--- include/robots/@SimonXaies/settings.php
+++ include/robots/@SimonXaies/settings.php
@@ -1,9 +1,9 @@
 <?php
 
-    return array(   'twitter'      =>  array(   'consumer_key'                  =>  '56luSIiWEQGT1QPQ1nYzHvASB',
-                                                'consumer_secret'               =>  'fX8cCrqSkRvkg9HY5ijKl9DrtAv0hfKKWqUumrYxQLRTR5EtRq',
-                                                'oauth_access_token'            =>  '39871064-djsBWWQCigu8PVsIVmwIqzOMPGN3CxdqkJ1UDEIc3',
-                                                'oauth_access_token_secret'     =>  'TDnOZpxiOvPaDsyf2tMAmQwCQUzz5aurHjKtTAbs9Oi4X',
+    return array(   'twitter'      =>  array(   'consumer_key'                  =>  '56luSIiWE----QPQ1nYzHvASB',
+                                                'consumer_secret'               =>  'fX8cCrqSkRvkg9HY----l9DrtAv0hfKKWqUumrYxQLRTR5EtRq',
+                                                'oauth_access_token'            =>  '39871064-djsBWWQCig-----IVmwIqzOMPGN3CxdqkJ1UDEIc3',
+                                                'oauth_access_token_secret'     =>  'TDnOZpxiOvPaDsyf2tMAm-----zz5aurHjKtTAbs9Oi4X',
                                                 'username'                      =>  'SimonXaies'
                                    ),
                     'robots'       =>   array(  'twitter'           =>  array('retweeter', 'newfollowers', 'thankfollowers', 'fairwellfollowers')
